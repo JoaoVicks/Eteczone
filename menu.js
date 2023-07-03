f9e98f434548667dbdb610b90479c5bd56837b35
João Pedro Vitorio Santos Souza <132296221+danoninho2209@users.noreply.github.com>
2023-07-02 22:42:49 -0300

    02/07

--- menu.js
+++ menu.js
@@ -2,3 +2,31 @@ window.addEventListener("scroll", function() {
     let header = document.querySelector('#inav');
     header.classList.toggle('rolagem', window.scrollY > 0);
 });
+//mudança de texto//
+
+// Array com os textos para serem exibidos
+var texts = [
+    "Não se trata apenas de ver o que ninguém\nviu, mas sim de pensar,o que ninguém ainda\npensou sobre o que todo mundo vê.\n- Albert Szent-Györgyi",
+    "A imaginação é mais importante que\n o conhecimento.- Albert Einstein",
+    "Toda verdade física é simbólica, e os símbolos,\nenquanto significativos,têm o poder de afetar\nnossa compreensão do mundo.\nNiels Bohr "
+];
+
+// Função para atualizar o texto
+function changeText() {
+    var textContainer = document.getElementById("text-container");
+    var currentIndex = texts.indexOf(textContainer.textContent);
+    var nextIndex = (currentIndex + 1) % texts.length; // Próximo índice no array
+
+    textContainer.classList.remove("fade-in");
+    textContainer.classList.add("fade-out");
+
+    setTimeout(function() {
+        textContainer.textContent = texts[nextIndex];
+        textContainer.style.whiteSpace = "pre";
+        textContainer.classList.remove("fade-out");
+        textContainer.classList.add("fade-in");
+    }, 1000); // Tempo correspondente à duração da transição em CSS (0.5s = 500ms)
+}
+
+// Chamar a função changeText a cada 10 segundos
+setInterval(changeText, 10000);
