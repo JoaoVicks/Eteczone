6e4e6892144688be69af2c7cf871f5d7d8902e1c
João Pedro Vitorio Santos Souza <132296221+danoninho2209@users.noreply.github.com>
2023-08-22 22:22:28 -0300

    22/08

---
+++ tabela.js
@@ -0,0 +1,10 @@
+function mostrar(){
+    let info = document.querySelector(".info");
+
+    info.style.display = 'block';
+};
+function sumir(){
+    let info = document.querySelector(".info");
+
+    info.style.display = 'none';
+}
