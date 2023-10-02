ccb3f96e4c39ec26a16e0c6790fe274754a07c37
Drey012 <132307408+Drey012@users.noreply.github.com>
2023-10-02 01:13:02 -0300

    Add files via upload

--- curling.js
+++ curling.js
@@ -11,7 +11,7 @@ function valor(){
     var V2 = parseFloat(document.getElementById("V2").value)
     var sentX = document.getElementById("eixox").value
     var sentY = document.getElementById("eixoy").value
-
+    if (M1 < 0 && V1 < 0 && M2 < 0 && V2 < 0){
     var x = Math.abs(M1*V1 / (M1+M2))
     var y = Math.abs(M2*V2 / (M1+M2))
     var V = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2))
@@ -22,36 +22,65 @@ function valor(){
     
     if (sentX == "E"){
         x= x*-1;
+
+        
+       
+
     }
     else {
     x=x
+    
     }
 
     if (sentY == "B"){
         y= y*-1
+      
     }
     else {
         y=y
+      
     }
     if (x > 0){
 if (y > 0){
     finalangle = refangle;
+    document.getElementById("VT").innerHTML = ` 
+    x = ${Math.abs(M1)} * ${V1} / (${M1} + ${M2}) = ${x}
+    y = ${Math.abs(M2)} * ${V2} / (${M1} + ${M2}) = ${y}
+    V = √${Math.pow(x, 2)} + ${Math.pow(y, 2)} = ${V}
+    angulo = tan⁻¹${Math.abs(y)} / ${Math.abs(x)} = ${finalangle}`
 }
 else{
     finalangle = 360 - refangle;
+    document.getElementById("VT").innerHTML = ` 
+        x = ${Math.abs(M1)} * ${V1} / (${M1} + ${M2}) = ${x}
+        y = (${Math.abs(M2)} * ${V2} / (${M1} + ${M2})) * -1 = ${y}
+        V = √${Math.pow(x, 2)} + ${Math.pow(y, 2)} = ${V}
+        angulo = tan⁻¹${Math.abs(y)} / ${Math.abs(x)} = ${finalangle}`
 }
 }
 else{
 if (y > 0){
     finalangle = 180 - refangle;
+    document.getElementById("VT").innerHTML = ` 
+        x = (${Math.abs(M1)} * ${V1} / (${M1} + ${M2})) * -1 = ${x}
+        y = ${Math.abs(M2)} * ${V2} / (${M1} + ${M2}) = ${y}
+        V = √${Math.pow(x, 2)} + ${Math.pow(y, 2)} = ${V}
+        angulo = tan⁻¹${Math.abs(y)} / ${Math.abs(x)} = ${finalangle}`
 }
 else{
     finalangle = 180 + refangle;
-}
+    document.getElementById("VT").innerHTML = ` 
+        x = (${Math.abs(M1)} * ${V1} / (${M1} + ${M2})) * -1 = ${x}
+        y = (${Math.abs(M2)} * ${V2} / (${M1} + ${M2})) * -1 = ${y}
+        V = √${Math.pow(x, 2)} + ${Math.pow(y, 2)} = ${V}
+        angulo = tan⁻¹${Math.abs(y)} / ${Math.abs(x)} = ${finalangle}`
+        }
+    }
 }
 
-document.getElementById("VT").innerHTML = ` Velocidade = ${V.toFixed(2)}`
-document.getElementById("ANG").innerHTML =  `Ângulo = ${finalangle.toFixed(2)}`
+else{
+    alert ("VOCÊ DEVE PREENCHER TODOS OS CAMPOS CORRETAMENTE")
+    }
  
 }
 let html = document.querySelector('html');
